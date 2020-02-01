75a572133a193ec6e15150888c3f52a7394ff386
khemrajneupane <shekharneupane@gmail.com>
2020-02-01 12:49:52 +0200

    eslint fix, backend routing conditional,frontend render hourly data in html table

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,29 +4,56 @@ const Koa = require('koa')
 const router = require('koa-router')()
 const fetch = require('node-fetch')
 const cors = require('kcors')
-
+//https://api.openweathermap.org/data/2.5/forecast?appid=a318e7cbd34562aff0648d5ef8c0d022&lat=35&lon=139&units=metric&cnt=7
 const appId = config.APPID || ''
+//const mapURI = config.MAP_ENDPOINT || 'http://api.openweathermap.org/data/2.5/'
+
 const mapURI = config.MAP_ENDPOINT || 'http://api.openweathermap.org/data/2.5'
 const targetCity = config.TARGET_CITY || 'Helsinki,fi'
-
+//let lat = 35
+//let lon = 139
 const port = config.PORT || 9000
 
 const app = new Koa()
 
 app.use(cors())
 
-const fetchWeather = async () => {
-    const endpoint = `${mapURI}/weather?q=${targetCity}&appid=${appId}&`
-    const response = await fetch(endpoint)
+//https://api.openweathermap.org/data/2.5/forecast?appid=a318e7cbd34562aff0648d5ef8c0d022&lat=35&lon=139&units=metric&cnt=7
 
+
+const fetchWeather = async ({lat = null, lon=null,prefix= 'weather'}) => {
+   
+    //const endpoint=`${mapURI}/weather?q=${targetCity}&appid=${appId}&`;
+    const noCords = `${mapURI}/${prefix}?q=${targetCity}&appid=${appId}`
+    const endpoint = `${mapURI}/${prefix}?appid=${appId}&lat=${lat}&lon=${lon}&units=metric&cnt=7`
+    const routingEquasion = lat ===null && lon === null?noCords:endpoint
+    const response = await fetch(routingEquasion)
+    
     return response ? response.json() : {}
 }
+/*
+router.get('/api/i', async (ctx) => {
+    const weatherData = await fetchWeather({ctx})
+    console.log(ctx)
+    ctx.type = 'application/json; charset=utf-8'
+    ctx.body = weatherData
+    return ctx.body
+});
+*/
 
 router.get('/api/weather', async ctx => {
-    const weatherData = await fetchWeather()
-
+    const weatherData = await fetchWeather({...ctx.request.query,prefix:'weather'})
+    console.log(weatherData)
     ctx.type = 'application/json; charset=utf-8'
-    ctx.body = weatherData.weather ? weatherData.weather[0] : {}
+    ctx.body = weatherData.weather ? weatherData.weather[0]:{}
+    //return ctx.body
+})
+router.get('/api/forecast', async ctx => {
+    const weatherData = await fetchWeather({...ctx.request.query,prefix:'forecast'})
+    console.log("weater data list: " , weatherData.list)
+    ctx.type = 'application/json; charset=utf-8'
+    ctx.body = weatherData
+    return ctx.body
 })
 
 app.use(router.routes())
--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,41 +1,92 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-// const baseURL = process.env.ENDPOINT;
-
-const getWeatherFromApi = async () => {
+const baseURL = process.env.baseURL || 'http://localhost:3003/api'
+console.log(process.env.KEY)
+const getWeatherFromApi = async (lat, lan) => {
     try {
-        const response = await fetch('http://localhost:3003/api/weather')
-        console.log(response)
-        return response.json()
+        const response = await fetch(`${baseURL}/forecast?lat=${lat}&lon=${lan}`)
+        const data = await response.json()
+        return data
     } catch (error) {
         console.error(error)
     }
-
     return {}
 }
 
-class Weather extends React.Component {
+const getPosition = () => new Promise((resolve, reject) => navigator.geolocation.getCurrentPosition(resolve, reject, { timeout: 5000 }))
+
+
+getPosition()
+    .then((position) => ({
+        lat: position.coords.latitude,
+        lon: position.coords.longitude,
+    }))
+    .catch((err) => console.error(err.message))
+
+const returnTime = (unixtime) => {
+    const hours = new Date(unixtime * 1000).getHours()
+    const minutes = new Date(unixtime * 1000).getMinutes()
+    return (`${hours}:${minutes}`)
+}
+
+export default class Weather extends React.Component {
     constructor(props) {
         super(props)
-
         this.state = {
-            icon: '',
+            icon: [],
+            description: [],
+            time: [],
         }
     }
 
     async UNSAFE_componentWillMount() {
-        const weather = await getWeatherFromApi()
-        this.setState({ icon: weather.icon.slice(0, -1) })
+        try {
+            const latLan = await getPosition()
+            const weather = await getWeatherFromApi(latLan.coords.latitude, latLan.coords.longitude)
+            const descriptions = weather.list.map((d) => d.weather.map((w) => w.description))
+            const times = weather.list.map((d) => d.dt)
+            const imgs = weather.list.map((d) => d.weather.map((w) => w.icon))
+            this.setState({
+                description: descriptions,
+                icon: imgs,
+                time: times,
+            })
+        } catch (error) {
+            console.error(error.message)
+        }
     }
 
+    styles = {
+
+        td: { width: '14%', backgroundColor: '#8C728A', height: '100px' },
+        th: { width: '14%', backgroundColor: '#728A8C', height: '100px' },
+
+    };
+
     render() {
-        const { icon } = this.state
+        const { icon, description, time } = this.state
+        const describe = description.map((d, index) => <td style={this.styles.td} key={index}>{d}</td>)
+        const hrMn = time.map((t, index) => <th style={this.styles.th} key={index}>{returnTime(t)}</th>)
+        const imageIcon = icon.map((i, index) => <th style={this.styles.th} key={index}><img src={`http://openweathermap.org/img/w/${i}.png`} alt="weatherIcon"/></th>)
 
         return (
-            <div className="icon">
-                
-                {icon && <img src={`/img/${icon}.svg`} alt="weatherimg" />}
+            <div>
+                <table>
+                    <thead>
+                        <tr>
+                            {hrMn}
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            {imageIcon}
+                        </tr>
+                        <tr>
+                            {describe}
+                        </tr>
+                    </tbody>
+                </table>
             </div>
         )
     }
